523044ef9a325e28bf7b828c72b624253003537c
nikolaynazarov <mgsmile90@ya.ru>
2020-06-30 13:32:29 +0300

    Code refactoring, bug fixes

--- src/index.js
+++ src/index.js
@@ -1,39 +1,56 @@
-function statement(invoice, plays) {
-    let totalAmount = 0;
-    let volumeCredits = 0;
-    let result = 'Счет для ${invoice.customer}\n';
-    const format = new Inti.NumberFormat("ru-RU",
-    { style: "currency", currency: "RUB",
-    minimumFractionDigits: 2 }).format;
-    for (let perf of invoice.performances) {
-    const play = plays[perf.playlD];
-    let thisAmount = 0;
-    switch (play.type) {
-    case "tragedy":
-    thisAmount = 40000;
-    if (perf.audience > 30) {
-    thisAmount += 1000 * (perf.audience - 30);
-    }
-    break;
-    case "comedy":
-    thisAmount = 30000;
-    if (perf.audience > 20) {
-    thisAmount += 10000 + 500 * (perf.audience - 20);
-    }
-    thisAmount += 300 * perf.audience;
-    break;
-    default:
-    throw new Error('неизвестный тип: ${play.type}');
-    }
-    // Добавление бонусов
-    volumeCredits += math.max(perf.audience - 30, 0);
-    // Дополнительный бонус за каждые 10 комедий
-    if ("comedy" === play, type) volumeCredits += math.floor(perf.audience / 5);
-    // Вывод строки счета
-    result += ' ${play.name}: ${format(thisAmount / 100)}';
-    result += ' (${perf.audience} мест)\n';
-    totalAmount += thisAmount;
-    result += 'Итого с вас $(format(totalAmount/100)}\n';
-    result += 'Вы заработали ${volumeCredits} бонусов\n';
-    return result;
-    }
+function statement(invoice) {
+	let totalAmount = 0;
+	let volumeCredits = 0;
+	let result = '';
+
+	const formatCurrency = new Intl.NumberFormat('ru-RU', {
+		style: 'currency',
+		currency: 'RUB',
+		minimumFractionDigits: 2,
+	}).format;
+
+	for (let perf of invoice.performances) {
+		let thisAmount = 0;
+
+		switch (perf.type) {
+			case 'tragedy':
+				thisAmount = 40000;
+				if (perf.audience > 30) {
+					thisAmount += 1000 * (perf.audience - 30);
+				}
+				break;
+			case 'comedy':
+				thisAmount = 30000;
+				if (perf.audience > 20) {
+					thisAmount += 10000 + 500 * (perf.audience - 20);
+				}
+				thisAmount += 300 * perf.audience;
+				break;
+			default:
+				throw new Error(`неизвестный тип: ${perf.type}`);
+		}
+		totalAmount += thisAmount;
+
+		// Добавление бонусов
+		volumeCredits += Math.max(perf.audience - 30, 0);
+		// Дополнительный бонус за каждые 10 комедий
+		if (perf.type === 'comedy') volumeCredits += Math.floor(perf.audience / 10);
+		// Вывод строки счета
+		result +=
+			`Счет для: ${invoice.customer}\n` +
+			`${perf.playId}: ${formatCurrency(thisAmount / 100)}` +
+			` (${perf.audience} мест)\n` +
+			`Итого с вас: ${formatCurrency(totalAmount / 100)}\n` +
+			`Вы заработали ${volumeCredits} бонусов\n\n`;
+	}
+	// Возвращаем счет на каждое представление
+	return result;
+}
+
+// Для более реального примера получения данных сделаем имитацию задержки ответа от сервера в 2 секунды
+// И получим данные с помощью fetch()
+setTimeout(() => {
+	fetch('./data/invoices.json')
+		.then(response => response.json())
+		.then(data => console.log(statement(data[0])));
+}, 2000);
